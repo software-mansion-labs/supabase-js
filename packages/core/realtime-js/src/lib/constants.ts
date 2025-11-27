c9f9573f9371189abec018ba5ea1affd0dd1c77c
Alan Guzek <alan.guzek@swmansion.com>
2025-11-27 14:46:50 +0100

    fix(realtime): cannot import form path

--- packages/core/realtime-js/src/lib/constants.ts
+++ packages/core/realtime-js/src/lib/constants.ts
@@ -1,5 +1,5 @@
 import { version } from './version'
-import * as PhoenixConstants from 'phoenix/priv/static/constants'
+import * as Phoenix from 'phoenix'
 
 export const DEFAULT_VERSION = `realtime-js/${version}`
 
@@ -14,17 +14,17 @@ export const DEFAULT_TIMEOUT = 10000
 export const WS_CLOSE_NORMAL = 1000
 export const MAX_PUSH_BUFFER_SIZE = 100
 
-export const SOCKET_STATES = PhoenixConstants.SOCKET_STATES
-export type SocketState = PhoenixConstants.SocketState
+export const SOCKET_STATES = Phoenix.SOCKET_STATES
+export type SocketState = Phoenix.SocketState
 
-export const CHANNEL_STATES = PhoenixConstants.CHANNEL_STATES
-export type ChannelState = PhoenixConstants.ChannelState
+export const CHANNEL_STATES = Phoenix.CHANNEL_STATES
+export type ChannelState = Phoenix.ChannelState
 
 export const CHANNEL_EVENTS = {
-  ...PhoenixConstants.CHANNEL_EVENTS,
+  ...Phoenix.CHANNEL_EVENTS,
   access_token: 'access_token',
 } as const
-export type ChannelEvent = PhoenixConstants.ChannelEvent | 'access_token'
+export type ChannelEvent = Phoenix.ChannelEvent | 'access_token'
 
 // TODO: Look at this after checking Longpoll transport
 export enum TRANSPORTS {
