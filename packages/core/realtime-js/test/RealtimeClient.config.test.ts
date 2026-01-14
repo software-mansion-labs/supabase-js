030b6b285250c6c0cfca1081ab598fa11e97857b
Alan Guzek <alan.guzek@swmansion.com>
2026-01-14 11:12:30 +0100

    feat(realtime): add test for choosing correct encoder

--- packages/core/realtime-js/test/RealtimeClient.config.test.ts
+++ packages/core/realtime-js/test/RealtimeClient.config.test.ts
@@ -2,6 +2,8 @@ import assert from 'assert'
 import { afterEach, beforeEach, describe, expect, test } from 'vitest'
 import RealtimeClient from '../src/RealtimeClient'
 import { setupRealtimeTest, TestSetup, DEFAULT_API_KEY } from './helpers/setup'
+import { VSN_1_0_0, VSN_2_0_0 } from '../src/lib/constants'
+import Serializer from '../src/lib/serializer'
 
 let testSetup: TestSetup
 
@@ -44,5 +46,52 @@ describe('endpointURL', () => {
       vsn: '2.0.0',
     })
     assert.equal(client.endpointURL(), `${testSetup.wssUrl}?apikey=${DEFAULT_API_KEY}&vsn=2.0.0`)
+  })
+})
+
+describe('encode and decode', () => {
+  const exampleMsg = { join_ref: '0', ref: '1', topic: 't', event: 'e', payload: { foo: 1 } }
+
+  let encodedMsg: any
+  let decodedMsg: any
+
+  beforeEach(() => {
+    encodedMsg = null
+    decodedMsg = null
+  })
+
+  test('are set to JSON if version is 1.0.0', () => {
+    const client = new RealtimeClient(testSetup.realtimeUrl, {
+      vsn: VSN_1_0_0,
+      params: { apikey: DEFAULT_API_KEY },
+    })
+
+    const encodedExample = JSON.stringify(exampleMsg)
+    client.encode(exampleMsg, (encoded) => (encodedMsg = encoded))
+    expect(encodedMsg).toStrictEqual(encodedExample)
+
+    const decodedExample = JSON.parse(encodedExample)
+    client.decode(encodedExample, (decoded) => (decodedMsg = decoded))
+    expect(decodedMsg).toStrictEqual(decodedExample)
+  })
+
+  test('are set to serializer if version is 2.0.0', () => {
+    const client = new RealtimeClient(testSetup.realtimeUrl, {
+      vsn: VSN_2_0_0,
+      params: { apikey: DEFAULT_API_KEY },
+    })
+
+    let encodedExample: string = ''
+    let decodedExample: Object = {}
+
+    const serializer = new Serializer()
+
+    serializer.encode(exampleMsg, (encoded) => (encodedExample = encoded as string))
+    client.encode(exampleMsg, (encoded) => (encodedMsg = encoded as string))
+    expect(encodedMsg).toStrictEqual(encodedExample)
+
+    serializer.decode(encodedExample, (decoded: Object) => (decodedExample = decoded))
+    client.decode(encodedExample, (decoded) => (decodedMsg = decoded))
+    expect(decodedMsg).toStrictEqual(decodedExample)
   })
 })
